af46ac68db7c7f9297f41e0fff129ace775249ee
Iroda-0220 <161613079+Iroda-0220@users.noreply.github.com>
2024-07-09 11:02:43 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+
+// if 1.9
+
+// let a = +prompt("Number");
+
+// if(a % 2 == 0){
+//     console.log('0')
+// }else{
+//     console.log('1')
+// }
+
+// if 1.8
+
+// let a = +prompt("Number");
+
+// if(a % 2 == 0){
+//     console.log('Juft')
+// }else{
+//     console.log('Toq')
+// }
+
+//  if 1.7
+
+// let a = +prompt("1 Number");
+// let b = +prompt("2 Number");
+
+// if(a > b){
+//     console.log(">")
+// }else if(a < b){
+//     console.log("<")
+// }else{
+//     console.log("=")
+// }
+
+// O'rtachasini topish
+
+// let a = +prompt("birinchi sonni kiritng");
+// let b = +prompt("ikkinchi sonni kiritng");
+// let c = +prompt("uchinchi sonni kiritng");
+
+// let midNum;
+
+// if(isNaN(a) || isNaN(b) || isNaN(c)){
+//     midNum = "Matn emas son kiriting"
+// }else if (a > b && a < c){
+//     midNum = a + "o'rtachasi"
+// }else if(b > a && b < c){
+//     midNum = b + "o'rtachasi"
+// }else{
+//     midNum = c + "o'rtachasi"
+// }
+
+// console.log(midNum)
+
+
+// Yosh 
+
+
+// let year = prompt("yoshingizni kiriting");
+
+// if(year > 0 && year < 18 ){ 
+//     console.log("yoshsiz oqishingiz kerak")
+// }else if (year > 18 && year < 50){
+//     console.log("ishlashingiz kerak")
+// }else if(year > 50 && year < 59){
+//     console.log("siz pensiyaga chiqasiz")
+// }else if(year > 59 && year < 150){
+//      console.log("pensionerga o'xshaysiz")
+// }else{
+//    console.log("Nimadir xato ketdi")
+// }
+
+// console.log(year)
+
+
